23c6f3e9cc9726cabf920ce7778a7a8bc7d49cb4
Patricio <patoestradac@gmail.com>
2023-05-08 17:22:15 -0500

    Errores menores corregidos

--- FundamentosBigData_Evidencia2Avance1.R
+++ FundamentosBigData_Evidencia2Avance1.R
@@ -35,7 +35,7 @@ for(j in 1:32)
   cat(delegaciones[j]," ",porcentajeXDelegacion,"%","\n",sep = '')
 }
 
-# Inciso C: Preguntas respondidas
+# Inciso C: Preguntas respondidas, colocando quienes hayan contestado menos al principio
 informacionOriginal <- read.csv("C:\\Users\\patoe\\Desktop\\Proyectos en R\\BigDataEvidencia2\\Evidencia2Avance1\\Resultados-MPSG-Open-Data-oct10-feb20.csv", check.names = FALSE, fileEncoding = "Latin1")
 preguntas <- c("Pregunta 1","Pregunta 2","Pregunta 3","Pregunta 4","Pregunta 5","Pregunta 6","Pregunta 7","Pregunta 8","Pregunta 9","Pregunta 10","Pregunta 11","Pregunta 12","Pregunta 13","Pregunta 14","Pregunta 15","Pregunta 16","Pregunta 17","Pregunta 18","Pregunta 19","Pregunta 20","Pregunta 21","Pregunta 22","Pregunta 23","Pregunta 24","Pregunta 25","Pregunta 26","Pregunta 27","Pregunta 28","Pregunta 29","Pregunta 30","Pregunta 31","Pregunta 32","Pregunta 33","Pregunta 34","Pregunta 35","Pregunta 36","Pregunta 37","Pregunta 38","Pregunta 39","Pregunta 40","Pregunta 41","Pregunta 42","Pregunta 43","Pregunta 44","Pregunta 45","Pregunta 46","Pregunta 47","Pregunta 48","Pregunta 49","Pregunta 50","Pregunta 51","Pregunta 52","Pregunta 53","Pregunta 54","Pregunta 55","Pregunta 56","Pregunta 57","Pregunta 58","Pregunta 59","Pregunta 60","Pregunta 61","Pregunta 62","Pregunta 63","Pregunta 64","Pregunta 65","Pregunta 66","Pregunta 67","Pregunta 68","Pregunta 69","Pregunta 70","Pregunta 71","Pregunta 72")
 vecesRespondidas <- c()
@@ -52,8 +52,7 @@ listapreguntas <- data.frame(preguntas, vecesRespondidas)
 
 print(listapreguntas[order(listapreguntas$vecesRespondidas, decreasing = FALSE),])
 
-#Inciso D: sadasd
-
+#Inciso D: Porcentaje de preguntas respondidas por localidad
 informacionOriginal <- read.csv("C:\\Users\\patoe\\Desktop\\Proyectos en R\\BigDataEvidencia2\\Evidencia2Avance1\\Resultados-MPSG-Open-Data-oct10-feb20.csv", check.names = FALSE, fileEncoding = "Latin1")
 localidades<- read.csv("C:\\Users\\patoe\\Desktop\\Proyectos en R\\BigDataEvidencia2\\Evidencia2Avance1\\Localidades.csv", check.names = FALSE, fileEncoding = "Latin1")
 informacionOriginal["Pregunta 70"][is.na(informacionOriginal["Pregunta 70"])]<-0
@@ -61,7 +60,7 @@ informacionOriginal["Pregunta 70"][is.na(informacionOriginal["Pregunta 70"])]<-0
 for (j in 1:nrow(localidades)) 
 {
   
-  tablaFIltradaXLocalidad <- informacionOriginal[informacionOriginal$Localidad == localidades[100,2],]
+  tablaFIltradaXLocalidad <- informacionOriginal[informacionOriginal$Localidad == localidades[j,2],]
   totalPreguntasRespondidasXLocalidad <- 0
   totalPreguntasRealizadasXLocalidad <- 74*nrow(tablaFIltradaXLocalidad)
   
